1ef2ebdfb460a5a167f468596d9a93cfd4564d8b
Brent.Voyles3 <Brent.Voyles3@gmail.com>
2024-07-14 11:02:28 -0400

    Added navbar style

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="#!">F<span>oo</span>dieSpot</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">About</a>
+                <a href="#!">Settings</a>
+            </div>
+            
+            
+</div>
+    )
+}
